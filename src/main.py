5b21eb7cbf0e5d9e4c7d6650bc8175eaca88f00f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-28 21:52:13 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,25 @@
+"""Module main.py"""
 import logging
 import os
 import sys
 
 
 def main():
+    """
+    Entry point.  Use this simple template as a starting point for
+    rapids.ai dependent, i.e., GPU (Graphics Processing Unit) accelerated, programs.
+
+    If run as-is, a sample 200,000 integers, within [1  16), will be generated, and
+    the frequency of each distinct number determined.
+
+    :return:
+    """
 
     # Logging
     logger = logging.getLogger(__name__)
     logger.info('CUDF')
 
+    # Numbers, frequency table.
     example = src.algorithms.example.Example()
     data = example.exc()
     logger.info(data.head())
